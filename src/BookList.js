492303eb4f885ab63c6ba4c7d1ed76f36019d7ce
Milton Bittencourt <miltonbst@gmail.com>
2018-06-13 20:20:01 -0300

    Creating BookShelf component

--- src/BookList.js
+++ src/BookList.js
@@ -5,9 +5,11 @@ class BookList extends Component {
     render () {
         return (
             <div className="list-books">
+            
             <div className="list-books-title">
-            <h1>MyReads</h1>
+                <h1>MyReads</h1>
             </div>
+
             <div className="list-books-content">
             <div>
                 <div className="bookshelf">
